9e0061e51775f91a7299d09c0b7df116ac8ee5d5
PXgamer <owzie123@gmail.com>
2017-05-25 15:51:35 +0100

    Removed old files for conversion

--- connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost', 'wwrg-torrents', 'TaJ35Nme8Wc7EYQ8', 'wwrg-torrents');
-if ($mysqli->connect_error) {
-    die('Error : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-}
